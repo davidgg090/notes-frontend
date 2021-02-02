7cb3ddab310ce3d9ce060a8bd66a0f861582edf0
davidgg090 <davidgg090@gmail.com>
2021-02-02 14:46:10 -0300

    card and list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoteListComponent } from './pages/note-list/note-list.component';
 import { MainLayaoutComponent } from './pages/main-layaout/main-layaout.component';
+import { NoteCardComponent } from './note-card/note-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NoteListComponent,
-    MainLayaoutComponent
+    MainLayaoutComponent,
+    NoteCardComponent
   ],
   imports: [
     BrowserModule,
