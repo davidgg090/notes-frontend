88785e898327feaf14976d072a6290cc4d285b08
davidgg090 <davidgg090@gmail.com>
2021-02-04 15:39:06 -0300

    animations

--- src/app/pages/note-list/note-list.component.ts
+++ src/app/pages/note-list/note-list.component.ts
@@ -1,3 +1,4 @@
+import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Note } from '../../shared/note.module';
 import { NotesService } from '../../shared/notes.service';
@@ -5,7 +6,68 @@ import { NotesService } from '../../shared/notes.service';
 @Component({
   selector: 'app-note-list',
   templateUrl: './note-list.component.html',
-  styleUrls: ['./note-list.component.scss']
+  styleUrls: ['./note-list.component.scss'],
+  animations: [
+    trigger('itemAnim', [
+      transition('void => *', [
+        style({
+          height:0,
+          opacity:0,
+          transform: 'scale(0.85)',
+          'margin-bottom': 0,
+          paddingTop:0,
+          paddingBottom:0,
+          paddingRight:0,
+          paddingLeft:0,
+        }),
+        animate('50ms', style({
+          height:'*',
+          'margin-bottom': '*',
+          paddingTop:'*',
+          paddingBottom:'*',
+          paddingRight:'*',
+          paddingLeft:'*',
+        })),
+        animate(100)
+      ]),
+      transition('* => void', [
+        animate(50, style({
+          transform:'scale(1.05)'
+        })),
+        animate(50, style({
+          transform:'scale(1)',
+          opacity:0.7
+        })),
+        animate('120ms ease-out', style({
+          opacity:0,
+          transform:'scale(0.68)'
+        })),
+        animate('150ms ease-out', style({
+          height:0,
+          'margin-bottom': 0,
+          paddingTop:0,
+          paddingBottom:0,
+          paddingRight:0,
+          paddingLeft:0,
+        }))
+      ])
+    ]),
+    trigger('listAnim', [
+      transition('* => *', [
+        query(':enter', [
+          style({
+            opacity:0,
+            height:0,
+          }),
+          stagger(100, [
+            animate('0.2s ease')
+          ])
+        ], {
+          optional: true
+        })
+      ])
+    ])
+  ]
 })
 export class NoteListComponent implements OnInit {
 
