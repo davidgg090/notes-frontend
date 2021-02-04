65d2bf6aee99c47c7b08f72a04ccd7676e51d66e
davidgg090 <davidgg090@gmail.com>
2021-02-04 16:02:00 -0300

    filter

--- src/app/pages/note-list/note-list.component.ts
+++ src/app/pages/note-list/note-list.component.ts
@@ -2,6 +2,7 @@ import { animate, query, stagger, style, transition, trigger } from '@angular/an
 import { Component, OnInit } from '@angular/core';
 import { Note } from '../../shared/note.module';
 import { NotesService } from '../../shared/notes.service';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-note-list',
@@ -72,17 +73,58 @@ import { NotesService } from '../../shared/notes.service';
 export class NoteListComponent implements OnInit {
 
   notes: Note[] = new Array<Note>();
+  filteredNotes : Note[] = new Array<Note>();
 
   constructor(private noteService: NotesService) { }
 
   ngOnInit(): void {
 
     this.notes = this.noteService.getAll();
+    this.filteredNotes = this.notes;
   }
 
   deleteNote(id: number){
     this.noteService.delete(id);
     
+  }
+
+  filter(query: string){
+    query = query.toLowerCase().trim();
+    let allResults: Note[] = Array<Note>();
+    let terms: string[] = query.split(' ');
+    terms = this.removeDuplicates(terms);
+
+
+    terms.forEach(term => {
+      let results: Note[] = this.revelantNotes(term);
+
+      allResults = [...allResults, ...results]
+    });
+
+    let uniqueResults = this.removeDuplicates(allResults);
+
+    this.filteredNotes = uniqueResults;
+
+
+  }
+
+  removeDuplicates(arr: Array<any>): Array<any>{
+    let uniqueResults: Set<any> = new Set<any>();
+    arr.forEach(e => uniqueResults.add(e));
+    return Array.from(uniqueResults)
+  }
+
+
+  revelantNotes(query: string): Array<Note>{
+    query = query.toLowerCase().trim();
+    let revelantNotes = this.notes.filter(note => {
+      if(note.body.toLowerCase().includes(query) || note.title.toLowerCase().includes(query)){
+        return true;
+      }
+      return false;
+    })
+
+    return revelantNotes;
   }
 
 }
